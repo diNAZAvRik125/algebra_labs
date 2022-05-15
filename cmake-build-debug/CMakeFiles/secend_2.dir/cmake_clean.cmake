07ecce95613844dd598380ed09cf66c869e8367f
Коновалов Назар Андреевич <n.konovalov1@g.nsu.ru>
2022-05-16 00:36:37 +0700

    Комментарий к ветке branch_name

---
+++ cmake-build-debug/CMakeFiles/secend_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secend_2.dir/main.cpp.obj"
+  "secend_2.exe"
+  "secend_2.exe.manifest"
+  "secend_2.lib"
+  "secend_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secend_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
